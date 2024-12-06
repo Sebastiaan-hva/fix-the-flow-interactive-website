a3acb2b93ad144e89499e0ca905cd03cef9c9df3
Sebastiaan <sebastiaan.hagoort@hva.nl>
2024-12-06 10:47:10 +0100

    comments geschreven js #3

--- script/script.js
+++ script/script.js
@@ -1,9 +1,15 @@
+// functie gebruikt jquery daar staat die $ voor.
+
+// functie om het programma te tonen
+
 $(document).ready(function () {
     $("#showContent").click(function () {
         $("#hideContent").show();
         $("#showContent").hide();
         $("#hiddenId").show();
     });
+
+// functie om het programma te verstoppen
 
     $("#hideContent").click(function () {
         $("#showContent").show();
@@ -12,15 +18,20 @@ $(document).ready(function () {
     });
 });
 
+
+// functie gedefineerd
 const audio = document.querySelector("audio");
 
+// play & pause button
 const playButton = document.getElementById("play-button");
 const pauseButton = document.getElementById("pause-button");
 
+// event listener voor click event om audio te starten
 playButton.addEventListener("click", () => {
     audio.play();
 });
 
+// event listener voor click event om audio te stoppen
 pauseButton.addEventListener("click", () => {
     audio.pause();
 });
